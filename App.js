965f4c367190e2bb1b91c57ce413cf67d5148e7d
FelipeCantu <felipecantujr@Felipes-MBP.lan>
2023-01-02 17:04:01 -0600

    nativewind

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className='flex-1 items-center justify-center bg-blue'>
+      <Text className='text-red-400'>Oo Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
