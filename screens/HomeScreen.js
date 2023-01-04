9ffd79fcf58fb223775e940f419b5e233acc8b4e
FelipeCantu <felipecantujr@Felipes-MBP.lan>
2023-01-03 20:21:33 -0600

    image

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    }, [])
+  })
   return (
-    <View>
-      <Text className='text-red-500'>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text className='text-blue-500'>HomeScreen</Text>
+      <View>
+        <Image 
+          source={{
+            uri: 'https://links.papareact.com/wru',
+          }}
+          className='h-7 w-7 bg-gray-300 rounded-full'
+        />
+      </View>
+    </SafeAreaView>
   )
 }
 
