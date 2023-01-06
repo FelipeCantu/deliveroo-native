2e16c224cc44fab06476891fc8e079f06c986b9f
FelipeCantu <felipecantujr@Felipes-MBP.lan>
2023-01-05 18:21:39 -0600

    heroicons

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import { View, Text, Image } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { UserIcon, ChevronDownIcon, SearchIcon, AdjustmentsIcon, } from 'react-native-heroicons/outline'
 
 const HomeScreen = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation()
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
@@ -12,15 +13,24 @@ const HomeScreen = () => {
   })
   return (
     <SafeAreaView>
-      <Text className='text-blue-500'>HomeScreen</Text>
-      <View>
+      <View className='flex-row pb-3 items-center mx-4 space-x-2'>
         <Image 
           source={{
             uri: 'https://links.papareact.com/wru',
           }}
           className='h-7 w-7 bg-gray-300 rounded-full'
         />
+        <View className='flex-1'>
+          <Text className='font-bold text-gray-400 text-xs'>
+            Deliver Now!
+          </Text>
+          <Text className='font-bold text-xl'>Current Location
+            <ChevronDownIcon size={20} color='#00ccbb' />
+          </Text>
+        </View>
+        <UserIcon size={35} color='#00ccbb' />
       </View>
+
     </SafeAreaView>
   )
 }
