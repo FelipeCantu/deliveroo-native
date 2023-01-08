c229c17a0c3bd9dbba30f97c76e66a702bc92a74
FelipeCantu <felipecantujr@Felipes-MBP.lan>
2023-01-08 09:02:24 -0600

    search bar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,36 +1,52 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TextInput, } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { UserIcon, ChevronDownIcon, SearchIcon, AdjustmentsIcon, } from 'react-native-heroicons/outline'
+import { UserIcon, ChevronDownIcon, MagnifyingGlassIcon, AdjustmentsHorizontalIcon } from 'react-native-heroicons/outline'
 
 const HomeScreen = () => {
   const navigation = useNavigation()
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
-    }, [])
-  })
-  return (
-    <SafeAreaView>
-      <View className='flex-row pb-3 items-center mx-4 space-x-2'>
-        <Image 
-          source={{
-            uri: 'https://links.papareact.com/wru',
-          }}
-          className='h-7 w-7 bg-gray-300 rounded-full'
-        />
-        <View className='flex-1'>
-          <Text className='font-bold text-gray-400 text-xs'>
-            Deliver Now!
-          </Text>
-          <Text className='font-bold text-xl'>Current Location
-            <ChevronDownIcon size={20} color='#00ccbb' />
-          </Text>
-        </View>
-        <UserIcon size={35} color='#00ccbb' />
-      </View>
+    })
+  }, [])
 
+  return (
+    <SafeAreaView className='bg-white pt-5'>
+        {/**Header */}
+        <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+          <Image 
+            source={{
+              uri: 'https://links.papareact.com/wru',
+            }}
+            className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+          />
+
+          <View className='flex-1'>
+            <Text className='font-bold text-gray-400 text-xs'>
+              Deliver Now!
+            </Text>
+            <Text className='font-bold text-xl'>
+              Current Location
+              <ChevronDownIcon size={20} color='#00ccbb' />
+            </Text>
+          </View>
+          <UserIcon size={35} color='#ooccbb' />
+        </View>
+        {/**Search */}
+        <View className='flex-row items-center space-x-2 pb-2 mx-4'>
+          <View className='flex-row flex-1 space-x-2 bg-gray-200 p-3'>
+            <MagnifyingGlassIcon color='gray' size={20} />
+            <TextInput 
+              placeholder='Restaurants and cusisins'
+              keyboardType='default'
+            />
+
+          </View>
+          <AdjustmentsHorizontalIcon color='#00ccbb' />
+        </View>
     </SafeAreaView>
   )
 }
