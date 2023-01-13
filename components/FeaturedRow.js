b34afe5bf7643505e2aaca75323c2cfe2d9f7b01
FelipeCantu <felipecantujr@Felipes-MBP.lan>
2023-01-12 19:32:45 -0600

    sanity2.0

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { ArrowRightIcon } from 'react-native-heroicons/outline'
 import RestaurantCard from './RestaurantCard'
 
-const FeaturedRow = ({ title, description, featuredCategory }) => {
+const FeaturedRow = ({ id, title, description }) => {
   return (
     <View>
       <View className='mt-4 flex-row items-center justify-between px-4'>
